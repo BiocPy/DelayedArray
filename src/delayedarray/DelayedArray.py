1f1b29ac3280bcd46d687b5095caab598c70f556
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-08-23 14:12:53 -0700

    Cleaned up the documentation.

--- src/delayedarray/DelayedArray.py
+++ src/delayedarray/DelayedArray.py
@@ -64,18 +64,19 @@ translate_ufunc_to_op_simple = set(
 
 class DelayedArray:
     """Array containing delayed operations.
-
-    This allows users to efficiently operate on large matrices without actually
-    evaluating the operation or creating new copies.
+    This is equivalent to the class of the same name from the R/Bioconductor package of the same name. 
+    It allows users to efficiently operate on large matrices without actually evaluating the operation or creating new copies;
+    instead, the operations will transparently return another DelayedArray instance containing the delayed operations,
+    which can be realized by calling :py:meth:`~numpy.array` or related methods.
 
     Attributes:
         seed:
-            Any array-like object with the ``shape`` and ``dtype`` properties.
-
-            This should also have methods for the
+            Any array-like object that satisfies the "seed contract". 
+            This means that it has the :py:attr:`~shape` and :py:attr:`~dtype` properties. 
+            It should also have methods for the
             :py:meth:`~delayedarray.interface.is_sparse`,
             :py:meth:`~delayedarray.interface.extract_dense_array`,
-            and (if ``is_sparse`` may return True)
+            and (if :py:meth:`~delayedarray.interface.is_sparse` could return True)
             :py:meth:`~delayedarray.interface.extract_sparse_array` generics.
     """
 
@@ -84,7 +85,7 @@ class DelayedArray:
 
     @property
     def shape(self) -> Tuple[int, ...]:
-        """Shape of the delayed array.
+        """Shape of the DelayedArray.
 
         Returns:
             Tuple[int, ...]: Tuple of integers containing the array shape along
@@ -94,14 +95,20 @@ class DelayedArray:
 
     @property
     def dtype(self) -> dtype:
-        """Type of the elements in the array.
+        """Type of the elements in the DelayedArray.
 
         Returns:
-            dtype: Type of the NumPy array containing the values of the non-zero elements.
+            dtype: NumPy type of the values. 
         """
         return self._seed.dtype
 
-    def __repr__(self):
+    def __repr__(self) -> str:
+        """Pretty-print this DelayedArray.
+        This uses :py:meth:`~numpy.array2string` and responds to all of its options.
+        
+        Returns:
+            str: String containing a prettified display of the array contents.
+        """
         total = 1
         for s in self._seed.shape:
             total *= s
@@ -133,21 +140,24 @@ class DelayedArray:
         return preamble + "\n" + converted
 
     # For NumPy:
-    def __array__(self):
+    def __array__(self) -> ndarray:
+        """Convert a DelayedArray to a NumPy array.
+
+        Returns:
+            ndarray: Array of the same type as :py:attr:`~dtype` and shape as :py:attr:`~shape`.
+            This is guaranteed to be in C-contiguous order and to not be a view on other data.
+        """
         full_indices = sanitize_indices([slice(None)] * len(self.shape), self.shape)
         return extract_dense_array(self._seed, (*full_indices,))
 
-    def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
+    def __array_ufunc__(self, ufunc, method, *inputs, **kwargs) -> "DelayedArray":
         """Interface with NumPy array methods.
-
-        Usage:
-
-        .. code-block:: python
-
-            np.sqrt(delayed_array)
+        This is used to implement mathematical operations like :py:meth:`~numpy.log`,
+        or to override operations between NumPy class instances and DelayedArrays where the former is on the left hand side. 
+        Check out the NumPy's ``__array_ufunc__`` `documentation <https://numpy.org/doc/stable/reference/arrays.classes.html#numpy.class.__array_ufunc__>`_ for more details.
 
         Returns:
-            An object with the same type as caller.
+            DelayedArray: A DelayedArray instance containing the requested delayed operation.
         """
 
         if ufunc.__name__ in translate_ufunc_to_op_with_args:
@@ -165,64 +175,230 @@ class DelayedArray:
 
         raise NotImplementedError(f"'{ufunc.__name__}' is not implemented!")
 
-    def __add__(self, other):
+    def __add__(self, other) -> "DelayedArray":
+        """
+        Add something to the right-hand-side of a DelayedArray.
+
+        Args:
+            other:
+                A numeric scalar or a NumPy array of length equal to the extent of the last dimension of the DelayedArray.
+
+        Returns:
+            DelayedArray: A DelayedArray containing the delayed addition operation.
+        """
         return wrap_isometric_with_args(self, other, op="+", right=True)
 
-    def __radd__(self, other):
+    def __radd__(self, other) -> "DelayedArray":
+        """
+        Add something to the left-hand-side of a DelayedArray.
+
+        Args:
+            other:
+                A numeric scalar.
+                In theory, this could also be a NumPy array of length equal to the extent of the last dimension of the DelayedArray,
+                but that is usually handled via :py:meth:`~__array_ufunc__`.
+
+        Returns:
+            DelayedArray: A DelayedArray containing the delayed addition operation.
+        """
         return wrap_isometric_with_args(self, other, op="+", right=False)
 
-    def __sub__(self, other):
+    def __sub__(self, other) -> "DelayedArray":
+        """
+        Subtract something from the right-hand-side of a DelayedArray.
+
+        Args:
+            other:
+                A numeric scalar or a NumPy array of length equal to the extent of the last dimension of the DelayedArray.
+
+        Returns:
+            DelayedArray: A DelayedArray containing the delayed subtraction operation.
+        """
         return wrap_isometric_with_args(self, other, op="-", right=True)
 
     def __rsub__(self, other):
+        """
+        Subtract a DelayedArray from something else.
+
+        Args:
+            other:
+                A numeric scalar.
+                In theory, this could also be a NumPy array of length equal to the extent of the last dimension of the DelayedArray,
+                but that is usually handled via :py:meth:`~__array_ufunc__`.
+
+        Returns:
+            DelayedArray: A DelayedArray containing the delayed subtraction operation.
+        """
         return wrap_isometric_with_args(self, other, op="-", right=False)
 
     def __mul__(self, other):
+        """
+        Multiply a DelayedArray with something on the right hand side.
+
+        Args:
+            other:
+                A numeric scalar or a NumPy array of length equal to the extent of the last dimension of the DelayedArray.
+
+        Returns:
+            DelayedArray: A DelayedArray containing the delayed multiplication operation.
+        """
         return wrap_isometric_with_args(self, other, op="*", right=True)
 
     def __rmul__(self, other):
+        """
+        Multiply a DelayedArray with something on the left hand side.
+
+        Args:
+            other:
+                A numeric scalar.
+                In theory, this could also be a NumPy array of length equal to the extent of the last dimension of the DelayedArray,
+                but that is usually handled via :py:meth:`~__array_ufunc__`.
+
+        Returns:
+            DelayedArray: A DelayedArray containing the delayed multiplication operation.
+        """
         return wrap_isometric_with_args(self, other, op="*", right=False)
 
     def __truediv__(self, other):
+        """
+        Divide a DelayedArray by something. 
+
+        Args:
+            other:
+                A numeric scalar or a NumPy array of length equal to the extent of the last dimension of the DelayedArray.
+
+        Returns:
+            DelayedArray: A DelayedArray containing the delayed division operation.
+        """
         return wrap_isometric_with_args(self, other, op="/", right=True)
 
     def __rtruediv__(self, other):
+        """
+        Divide something by a DelayedArray.
+
+        Args:
+            other:
+                A numeric scalar.
+                In theory, this could also be a NumPy array of length equal to the extent of the last dimension of the DelayedArray,
+                but that is usually handled via :py:meth:`~__array_ufunc__`.
+
+        Returns:
+            DelayedArray: A DelayedArray containing the delayed division operation.
+        """
         return wrap_isometric_with_args(self, other, op="/", right=False)
 
     def __mod__(self, other):
+        """
+        Take the remainder after dividing a DelayedArray by something. 
+
+        Args:
+            other:
+                A numeric scalar or a NumPy array of length equal to the extent of the last dimension of the DelayedArray.
+
+        Returns:
+            DelayedArray: A DelayedArray containing the delayed modulo operation.
+        """
         return wrap_isometric_with_args(self, other, op="%", right=True)
 
     def __rmod__(self, other):
+        """
+        Take the remainder after dividing something by a DelayedArray. 
+
+        Args:
+            other:
+                A numeric scalar.
+                In theory, this could also be a NumPy array of length equal to the extent of the last dimension of the DelayedArray,
+                but that is usually handled via :py:meth:`~__array_ufunc__`.
+
+        Returns:
+            DelayedArray: A DelayedArray containing the delayed modulo operation.
+        """
         return wrap_isometric_with_args(self, other, op="%", right=False)
 
     def __floordiv__(self, other):
+        """
+        Divide a DelayedArray by something and take the floor.
+
+        Args:
+            other:
+                A numeric scalar or a NumPy array of length equal to the extent of the last dimension of the DelayedArray.
+
+        Returns:
+            DelayedArray: A DelayedArray containing the delayed floor division operation.
+        """
         return wrap_isometric_with_args(self, other, op="//", right=True)
 
     def __rfloordiv__(self, other):
+        """
+        Divide something by a DelayedArray and take the floor.
+
+        Args:
+            other:
+                A numeric scalar.
+                In theory, this could also be a NumPy array of length equal to the extent of the last dimension of the DelayedArray,
+                but that is usually handled via :py:meth:`~__array_ufunc__`.
+
+        Returns:
+            DelayedArray: A DelayedArray containing the delayed floor division operation.
+        """
         return wrap_isometric_with_args(self, other, op="//", right=False)
 
     def __pow__(self, other):
+        """
+        Raise a DelayedArray to the power of something.
+
+        Args:
+            other:
+                A numeric scalar or a NumPy array of length equal to the extent of the last dimension of the DelayedArray.
+
+        Returns:
+            DelayedArray: A DelayedArray containing the delayed power operation.
+        """
         return wrap_isometric_with_args(self, other, op="**", right=True)
 
     def __rpow__(self, other):
+        """
+        Raise something to the power of the contents of a DelayedArray. 
+
+        Args:
+            other:
+                A numeric scalar.
+                In theory, this could also be a NumPy array of length equal to the extent of the last dimension of the DelayedArray,
+                but that is usually handled via :py:meth:`~__array_ufunc__`.
+
+        Returns:
+            DelayedArray: A DelayedArray containing the delayed power operation.
+        """
         return wrap_isometric_with_args(self, other, op="**", right=False)
 
     def __neg__(self):
+        """
+        Negate the contents of a DelayedArray.
+
+        Returns:
+            DelayedArray: A DelayedArray containing the delayed negation.
+        """
         return wrap_isometric_with_args(self, 0, op="-", right=False)
 
     def __abs__(self):
+        """
+        Take the absolute value of the contents of a DelayedArray.
+
+        Returns:
+            DelayedArray: A DelayedArray containing the delayed absolute value operation.
+        """
         return DelayedArray(UnaryIsometricOpSimple(self._seed, op="abs"))
 
     def __getitem__(self, args: Tuple[Union[slice, Sequence], ...]) -> "DelayedArray":
-        """Create a delayed subset wrapper around this array.
+        """Take a subset of this DelayedArray.
+        Unlike NumPy, the subset will be an outer product of the per-dimension indices defined in ``args``;
+        this aligns with the behavior of subsetting in R, and is equivalent to using NumPy's :py:meth:`~numpy.ix_` function.
 
         Args:
-            args (Tuple[Union[slice, Sequence], ...]): A :py:class`tuple` defining the
-                positions of the array to access along each dimension.
-
-                Each element in ``args`` may be a :py:func:`slice` object or
-                a list of integer indices. The length of the tuple
-                must not exceed the number of dimensions in the array.
+            args (Tuple[Union[slice, Sequence], ...]): 
+                A :py:class:`tuple` of length equal to the dimensionality of this DelayedArray.
+                Each entry should contain a sequence of integer indices (e.g., a list, :py:class:`~numpy.ndarray` or :py:func:`slice`),
+                specifying the elements of the corresponding dimension to extract.
 
         Raises:
             ValueError: If ``args`` contain more dimensions than the shape of the array.
