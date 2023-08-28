1ea7e618232c66ddcac31100db912516a4bb3cf2
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-08-28 09:24:23 -0700

    Syntax fix for old python?

--- src/delayedarray/DelayedArray.py
+++ src/delayedarray/DelayedArray.py
@@ -753,7 +753,7 @@ class DelayedArray:
             return DelayedArray(Subset(self._seed, (*sanitized,)))
 
         # WHATEVER. Fuck this shit. Just do whatever.
-        test = self.as_dask_array()[*args]
+        test = self.as_dask_array()[(..., *args)]
         if len(test.shape) == ndim:
             raise NotImplementedError(
                 "Oops. Looks like the DelayedArray doesn't correctly handle this combination of index types, but it probably should. Consider filing an issue in at https://github.com/BiocPy/DelayedArray/issues."
