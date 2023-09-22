8f27a7ca2db8f3fd9f4d1aac8195b821ccbdadcb
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-09-22 09:22:00 -0700

    Added utility to guess block sizes for iteration.
    This is basically equivalent to DelayedArray's row/colAutoGrids.

--- src/delayedarray/utils.py
+++ src/delayedarray/utils.py
@@ -177,3 +177,39 @@ def chunk_shape(seed) -> Tuple[int]:
 
     # Guess we should return something.
     return seed.shape
+
+
+def guess_iteration_block_size(seed, dimension: int, memory: int = 10000000) -> int:
+    """Guess the best block size for iterating over the matrix on a certain
+    dimension.  This assumes that, in each iteration, an entire block of
+    observations is extracted involving the full extent of all dimensions other
+    than the one being iterated over. This block is used for processing before
+    extracting the next block of elements.
+
+    Args:
+        seed: Any seed object.
+
+        dimension: Dimension to iterate over.
+
+        memory: Available memory in bytes, to hold a single block in memory.
+
+    Returns:
+        Size of the block on the iteration dimension.
+    """
+    num_elements = memory / seed.dtype.itemsize
+    shape = seed.shape
+
+    prod_other = 1
+    for i, s in enumerate(shape):
+        if i != dimension:
+            prod_other *= s 
+
+    ideal = int(num_elements / prod_other)
+    if ideal == 0:
+        return 1
+
+    curdim = chunk_shape(seed)[dimension]
+    if ideal <= curdim:
+        return ideal
+
+    return int(ideal / curdim) * curdim
