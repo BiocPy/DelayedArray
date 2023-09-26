c6e91a0468bd34ac1ecb40a951adf6b8e1428608
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-09-26 10:00:28 -0700

    Added more extractor methods for scipy sparse matrix.

--- src/delayedarray/extract_sparse_array.py
+++ src/delayedarray/extract_sparse_array.py
@@ -42,7 +42,7 @@ def extract_sparse_array(x: Any, subset: Optional[Tuple[Sequence[int]]] = None) 
 
 
 @extract_sparse_array.register
-def extract_sparse_array_SparseNdarray(x: ndarray, subset: Optional[Tuple[Sequence[int]]] = None):
+def extract_sparse_array_SparseNdarray(x: SparseNdarray, subset: Optional[Tuple[Sequence[int]]] = None):
     if _is_subset_noop(x.shape, subset):
         subset = None
     if subset is None:
@@ -61,6 +61,12 @@ except:
 
 
 if has_sparse:
+    def _set_empty_contents(contents):
+        for x in contents:
+            if x is not None:
+                return contents
+        return contents
+
     @extract_sparse_array.register
     def extract_sparse_array_csc_matrix(x: scipy.sparse.csc_matrix, subset: Optional[Tuple[Sequence[int]]] = None):
         if subset is None:
@@ -68,15 +74,14 @@ if has_sparse:
 
         final_shape = [len(s) for s in subset]
         new_contents = None
-        rowsub = subset[0]
-        colsub = subset[1]
-        row_consecutive = _is_subset_consecutive(rowsub)
 
-        if final_shape[0] != 0 or final_shape[1] != 0:
+        if final_shape[0] != 0 and final_shape[1] != 0:
+            rowsub = subset[0]
+            row_consecutive = _is_subset_consecutive(rowsub)
             first = rowsub[0]
             last = rowsub[-1] + 1
-            new_contents = []
 
+            new_contents = []
             for ci in subset[1]:
                 start_pos = x.indptr[ci]
                 end_pos = x.indptr[ci + 1]
@@ -99,15 +104,15 @@ if has_sparse:
                     new_val = []
                     new_idx = []
                     pos = 0
-                    for ri in range(start_pos, end_pos):
-                        current = x.indices[ri]
-                        while pos < len(rowsub) and current > rowsub[pos]:
+                    for p in range(start_pos, end_pos):
+                        ri = x.indices[p]
+                        while pos < len(rowsub) and ri > rowsub[pos]:
                             pos += 1
                         if pos == len(rowsub):
                             break
-                        if current == rowsub[pos]:
-                            new_idx.append(current - first)
-                            new_val.append(x.data[ri])
+                        if ri == rowsub[pos]:
+                            new_idx.append(pos)
+                            new_val.append(x.data[p])
                             pos += 1
 
                     if len(new_val):
@@ -115,4 +120,108 @@ if has_sparse:
                     else:
                         new_contents.append(None)
 
-        return SparseNdarray((*final_shape,), new_contents, dtype=x.dtype, index_dtype=x.indices.dtype)
+            new_contents = _set_empty_contents(new_contents)
+
+        return SparseNdarray((*final_shape,), new_contents, dtype=x.dtype, index_dtype=x.indices.dtype, check=False)
+
+    @extract_sparse_array.register
+    def extract_sparse_array_csr_matrix(x: scipy.sparse.csr_matrix, subset: Optional[Tuple[Sequence[int]]] = None):
+        if subset is None:
+            subset = _spawn_indices(x.shape)
+
+        final_shape = [len(s) for s in subset]
+        new_contents = None
+
+        if final_shape[0] != 0 and final_shape[1] != 0:
+            colsub = subset[1]
+            col_consecutive = _is_subset_consecutive(colsub)
+            cfirst = colsub[0]
+            clast = colsub[-1] + 1
+
+            new_contents = []
+            for i in range(len(colsub)):
+                new_contents.append(([], []))
+
+            for rpos, ri in enumerate(subset[0]):
+                start_pos = x.indptr[ri]
+                end_pos = x.indptr[ri + 1]
+                if cfirst != 0:
+                    start_pos = bisect_left(x.indices, cfirst, lo=start_pos, hi=end_pos)
+
+                if col_consecutive:
+                    if clast != x.shape[1]:
+                        end_pos = bisect_left(x.indices, clast, lo=start_pos, hi=end_pos)
+
+                    for p in range(start_pos, end_pos):
+                        ci = x.indices[p]
+                        idx, val = new_contents[ci - cfirst]
+                        idx.append(rpos)
+                        val.append(x.data[p])
+
+                else:
+                    cpos = 0
+                    for p in range(start_pos, end_pos):
+                        ci = x.indices[p]
+                        while cpos < len(colsub) and ci > colsub[cpos]:
+                            cpos += 1
+                        if cpos == len(colsub):
+                            break
+                        if ci == colsub[cpos]:
+                            idx, val = new_contents[cpos]
+                            idx.append(rpos)
+                            val.append(x.data[p])
+                            pos += 1
+
+            for i in range(len(new_contents)):
+                idx, val = new_contents[i]
+                if len(idx):
+                    new_contents[i] = (array(idx, dtype=x.indices.dtype), array(val, dtype=x.dtype))) 
+                else:
+                    new_contents[i] = None
+
+            new_contents = _set_empty_contents(new_contents)
+
+        return SparseNdarray((*final_shape,), new_contents, dtype=x.dtype, index_dtype=x.indices.dtype, check=False)
+
+    @extract_sparse_array.register
+    def extract_sparse_array_coo_matrix(x: scipy.sparse.coo_matrix, subset: Optional[Tuple[Sequence[int]]] = None):
+        if subset is None:
+            subset = _spawn_indices(x.shape)
+
+        final_shape = [len(s) for s in subset]
+        new_contents = None
+
+        if final_shape[0] != 0 and final_shape[1] != 0:
+            new_contents = []
+            for i in range(len(colsub)):
+                new_contents.append([])
+
+            in_row = {}
+            for i, x in enumerate(subset[0]):
+                in_row[x] = i
+
+            in_col = {}
+            for i, x in enumerate(subset[1]):
+                in_col[x] = i
+
+            for i, v in enumerate(x.data):
+                r = x.row[i]
+                c = x.col[i]
+                if r in in_row and c in in_col:
+                    new_contents[in_col[c]].append((in_row[r], v))
+
+            for i, con in new_contents:
+                if len(con):
+                    con.sort()
+                    idx = ndarray(len(con), dtype=x.row.dtype)
+                    val = ndarray(len(con), dtype=x.data.dtype)
+                    for j, x in enumerate(con):
+                        idx[j] = x[0]
+                        val[j] = x[1]
+                    new_contents[i] = (idx, val)
+                else:
+                    new_contents[i] = None
+
+            new_contents = _set_empty_contents(new_contents)
+
+        return SparseNdarray((*final_shape,), new_contents, dtype=x.dtype, index_dtype=x.row.dtype, check=False)
--- tests/test_SparseNdarray.py
+++ tests/test_SparseNdarray.py
@@ -9,7 +9,6 @@ import numpy
 #######################################################
 
 import random
-from delayedarray.SparseNdarray import _extract_dense_array_from_SparseNdarray, _extract_sparse_array_from_SparseNdarray
 
 def mock_SparseNdarray_contents(shape, density1=0.5, density2=0.5, lower=-1, upper=1, dtype=numpy.float64):
     if len(shape) == 1:
@@ -208,19 +207,19 @@ def test_SparseNdarray_extract_dense_array_3d():
 
     # Sliced extraction.
     slices = (slice(2, 15, 3), slice(0, 20, 2), slice(4, 8))
-    sliced = _extract_dense_array_from_SparseNdarray(y, slices2ranges(slices, test_shape))
+    sliced = delayedarray.extract_dense_array(y, slices2ranges(slices, test_shape))
     assert (sliced == output[(..., *slices)]).all()
 
     slices = (slice(None), slice(0, 20, 2), slice(None))
-    sliced = _extract_dense_array_from_SparseNdarray(y, slices2ranges(slices, test_shape))
+    sliced = delayedarray.extract_dense_array(y, slices2ranges(slices, test_shape))
     assert (sliced == output[(..., *slices)]).all()
 
     slices = (slice(None), slice(None), slice(0, 8, 2))
-    sliced = _extract_dense_array_from_SparseNdarray(y, slices2ranges(slices, test_shape))
+    sliced = delayedarray.extract_dense_array(y, slices2ranges(slices, test_shape))
     assert (sliced == output[(..., *slices)]).all()
 
     slices = (slice(10, 30), slice(None), slice(None))
-    sliced = _extract_dense_array_from_SparseNdarray(y, slices2ranges(slices, test_shape))
+    sliced = delayedarray.extract_dense_array(y, slices2ranges(slices, test_shape))
     assert (sliced == output[(..., *slices)]).all()
 
 
@@ -235,15 +234,15 @@ def test_SparseNdarray_extract_dense_array_2d():
 
     # Sliced extraction.
     slices = (slice(5, 48, 5), slice(0, 90, 3))
-    sliced = _extract_dense_array_from_SparseNdarray(y, slices2ranges(slices, test_shape))
+    sliced = delayedarray.extract_dense_array(y, slices2ranges(slices, test_shape))
     assert (sliced == output[(..., *slices)]).all()
 
     slices = (slice(20, 30), slice(None))
-    sliced = _extract_dense_array_from_SparseNdarray(y, slices2ranges(slices, test_shape))
+    sliced = delayedarray.extract_dense_array(y, slices2ranges(slices, test_shape))
     assert (sliced == output[(..., *slices)]).all()
 
     slices = (slice(None), slice(10, 80))
-    sliced = _extract_dense_array_from_SparseNdarray(y, slices2ranges(slices, test_shape))
+    sliced = delayedarray.extract_dense_array(y, slices2ranges(slices, test_shape))
     assert (sliced == output[(..., *slices)]).all()
 
 
@@ -259,7 +258,7 @@ def test_SparseNdarray_extract_dense_array_1d():
 
     # Sliced extraction.
     slices = (slice(5, 90, 7),)
-    sliced = _extract_dense_array_from_SparseNdarray(y, slices2ranges(slices, test_shape))
+    sliced = delayedarray.extract_dense_array(y, slices2ranges(slices, test_shape))
     assert (sliced == output[(..., *slices)]).all()
 
 
@@ -345,12 +344,12 @@ def test_SparseNdarray_int_type():
     assert y.dtype == numpy.int16
 
     full_indices = [range(d) for d in test_shape]
-    dout = _extract_dense_array_from_SparseNdarray(y, full_indices)
+    dout = delayedarray.extract_dense_array(y, full_indices)
     assert dout.dtype == numpy.int16
     ref = convert_SparseNdarray_to_numpy(y)
     assert (dout == ref).all()
 
-    spout = _extract_sparse_array_from_SparseNdarray(y, full_indices)
+    spout = delayedarray.extract_sparse_array(y, full_indices)
     assert spout.dtype == numpy.int16
     assert (convert_SparseNdarray_to_numpy(spout) == ref).all()
 
@@ -362,16 +361,16 @@ def test_SparseNdarray_empty():
     assert y.dtype == numpy.uint32
 
     full_indices = [range(d) for d in test_shape]
-    dout = _extract_dense_array_from_SparseNdarray(y, full_indices)
+    dout = delayedarray.extract_dense_array(y, full_indices)
     assert (dout == numpy.zeros(test_shape)).all()
-    dout = _extract_dense_array_from_SparseNdarray(y, ([1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11, 12]))
+    dout = delayedarray.extract_dense_array(y, ([1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11, 12]))
     assert (dout == numpy.zeros((3, 4, 5))).all()
 
-    spout = _extract_sparse_array_from_SparseNdarray(y, full_indices)
+    spout = delayedarray.extract_sparse_array(y, full_indices)
     assert spout._contents is None
     assert spout.shape == test_shape
     assert spout.dtype == numpy.uint32
-    spout = _extract_sparse_array_from_SparseNdarray(y, ([1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11, 12]))
+    spout = delayedarray.extract_sparse_array(y, ([1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11, 12]))
     assert spout.shape == (3, 4, 5)
 
 
